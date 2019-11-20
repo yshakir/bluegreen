37893cef86ab8410e071b63a6cb5f7706bfc6391
yshakir <32714937+yshakir@users.noreply.github.com>
2019-11-20 15:47:17 -0500

    Update image.php

--- image.php
+++ image.php
@@ -3,7 +3,8 @@
 $im = imagecreatetruecolor(200, 200);
 $white = imagecolorallocate($im, 255, 255, 255);
 // Line reserved to trigger the webhook in the lab. 
-$deployment = getenv("COLOR");
+// $deployment = getenv("COLOR");
+$deployment = 'green';
 
 if ( $deployment == 'blue') {
   $color = imagecolorallocate($im, 0, 0, 255);
